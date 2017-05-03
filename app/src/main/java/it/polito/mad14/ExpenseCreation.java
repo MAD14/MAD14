dbc2bcdb6b7bca587bb653827af97db16e41426b
marcocerutti93 <marcocerutti93@gmail.com>
2017-05-03 19:02:45 +0200

    03/05 ore 19.00
    Expense Creation permette di fare una foto della spesaeffettuata (ad
    esempio lo scontrino). ancora work in progress la scrittura sul database

--- app/src/main/java/it/polito/mad14/ExpenseCreation.java
+++ app/src/main/java/it/polito/mad14/ExpenseCreation.java
@@ -1,20 +1,34 @@
 package it.polito.mad14;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Base64;
 import android.view.View;
 import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ListView;
+import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+
+import java.io.ByteArrayOutputStream;
 
 public class ExpenseCreation extends AppCompatActivity implements View.OnClickListener{
 
     private Button bt;
+    private ImageButton getExpenseImage;
+    private Bitmap expenseImageBitmap;
+    private String encodedExpenseImage;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,13 +37,31 @@ public class ExpenseCreation extends AppCompatActivity implements View.OnClickLi
         bt = (Button) findViewById(R.id.expense_button);
         bt.setOnClickListener(this);
 
+        getExpenseImage = (ImageButton) findViewById(R.id.insert_image);
+        getExpenseImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(cameraIntent, 0);
+            }
+        });
+
     }
 
 
     public void onClick(View v){
         //TODO sistemare che l'autore è l'utente stesso
         //TODO scrittura su firebase
-        String et_author = "me stesso";
+
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        String name = user.getDisplayName();
+        String email = user.getEmail();
+        String uid = user.getUid();
+        Toast.makeText(
+                ExpenseCreation.this, name + "\n" + email + "\n" + uid,Toast.LENGTH_SHORT).show();
+
+
+        /*String et_author = "me stesso";
         EditText et_name = (EditText)findViewById(R.id.expense_name);
         EditText et_description = (EditText)findViewById(R.id.expense_description);
         EditText et_import = (EditText)findViewById(R.id.expense_import);
@@ -54,13 +86,21 @@ public class ExpenseCreation extends AppCompatActivity implements View.OnClickLi
         intent.putExtra("import",et_import.getText().toString());
         intent.putExtra("description",et_description.getText().toString());
         setResult(RESULT_OK, intent);
-        finish();
-
-
-
+        finish();*/
     }
 
-    public void onClickImage(View v){
-        //TODO: inserire l'immagine della spesa
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK){
+            expenseImageBitmap = (Bitmap) data.getExtras().get("data");
+            BitmapDrawable bDrawable = new BitmapDrawable(getApplicationContext().getResources(),expenseImageBitmap);
+            getExpenseImage.setBackgroundDrawable(bDrawable);
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            expenseImageBitmap.compress(Bitmap.CompressFormat.JPEG,100,baos);
+            byte[] byteArrayImage = baos.toByteArray();
+            encodedExpenseImage = Base64.encodeToString(byteArrayImage, Base64.DEFAULT);
+        }
     }
+
 }
--- app/src/main/java/it/polito/mad14/NewGroupActivityPhase1.java
+++ app/src/main/java/it/polito/mad14/NewGroupActivityPhase1.java
@@ -7,6 +7,7 @@ import android.graphics.BitmapFactory;
 import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -25,6 +26,7 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 import java.io.ByteArrayOutputStream;
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.HashMap;
 import java.util.Map;
@@ -58,7 +60,7 @@ public class NewGroupActivityPhase1 extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
                 intent.setType("image/*");
-                startActivityForResult(intent, 1);
+                startActivityForResult(intent, 0);
             }
         });
 
